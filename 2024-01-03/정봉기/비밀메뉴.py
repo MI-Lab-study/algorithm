1c75f9945054c28526ec3934e048079e998caad4
JB0527 <qhdrl527@naver.com>
2024-01-03 15:13:50 +0900

    정봉기 코드

---
+++ 2024-01-03/정봉기/비밀메뉴.py
@@ -0,0 +1,9 @@
+#미완성
+
+import sys
+#m개의 버튼조작
+#K개의 버튼
+#사용자는 n개가 주어짐
+m,n,k = map(int,input().split())
+listsecret = [list(map(int, input().strip()))] 
+listuser = [list(map(int, input().strip()))]
